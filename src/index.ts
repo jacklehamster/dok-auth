a5d079ba244158717d7ef56da08f793d6fe0416f
Jack Le Hamster <jacklehamster@gmail.com>
2024-07-27 21:40:46 -0700

    Added new functionality to create an AuthManager with different configurations.
    - Added import for DataClient from "@dobuki/data-client".
    - Added import for Config as NewgroundsConfig from "medal-popup".
    - Created interface Props for secret and newgrounds configurations.
    - Modified createAuthManager function to accept secretConfig and newgroundsConfig.
    - Used the new configurations to create an AuthManager.
    
    Maybe we should consider having a "config" folder to keep all these configuration files together. Also, the function createAuthManager seems a bit long. We should consider refactoring it to make it more concise and readable.
    
    Looks like someone's trying to become the master of configurations! Keep it up, Config King!
    
    Files:
    M  src/index.ts
    
    Committed using auto-commit: Sat Jul 27 2024 21:40:46 GMT-0700 (Pacific Daylight Time)
    Autocommit project: https://www.npmjs.com/package/@dobuki/autocommit
    All the lame jokes were generated by AI.

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,31 @@ import { SecretAuthenticator } from "./authenticators/secret/secret-authenticato
 import { Authenticator } from "./authenticators/authenticator";
 import { Payload } from "./auth/Payload";
 import { TokenResult } from "./auth/TokenResult";
+import { DataClient } from "@dobuki/data-client";
+import { Config as NewgroundsConfig } from "medal-popup";
 
 export { AuthManager, AuthProvider, NewgroundsAuthenticator, SecretAuthenticator, Authenticator, Payload, TokenResult };
+
+interface Props {
+  secretConfig?: {
+    secretKey?: string;
+    secret: string;  
+  };
+  newgroundsConfig?: NewgroundsConfig;
+}
+
+export function createAuthManager(dataClient: DataClient, {
+  secretConfig,
+  newgroundsConfig,
+}: Props): AuthManager {
+  const provider = new AuthProvider(dataClient);
+  return new AuthManager(provider, [
+    ...secretConfig ? [new SecretAuthenticator({
+      secretKey: secretConfig.secretKey,
+      secretWord: secretConfig.secret,
+    })] : [],
+    ...newgroundsConfig ? [
+      new NewgroundsAuthenticator(newgroundsConfig),
+    ] : [],
+  ]);
+}
